9a34f5961327320a7f2b602719d0886fde56cbab
ecthon <ecthon@gmail.com>
2024-07-17 09:31:28 -0400

    feature/app: create functions to: add email, get input value, clear input field and remove email from list

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import { ArrowRight, MapPin, Calendar, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 export function App() {
   const [ isGuestInputOpen, setIsGuestInputOpen ] = useState(false)
   const [ isGuestModalOpen, setIsModalInputOpen ] = useState(false)
+  const [ emailsToInvite, setEmailToInvite ] = useState([
+    'ecthon@gmail.com'
+  ])
 
   function openGuestsInput () {
     setIsGuestInputOpen(true)
@@ -19,6 +22,41 @@ export function App() {
 
   function closeGuestModal () {
     setIsModalInputOpen(false)
+  }
+
+  function addNewEmailToInvite (event: FormEvent<HTMLFormElement>) {
+    // A linha a baixo preveni o comportament padrão do formulário
+    // que, após enviar o valor, a página é redirecionda.
+    // Usando a função "preventDefault", eu envio o valor (enter/click) e continuo na
+    // mesma página podendo adicionar mais emails.
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    // Se o campo de email estiver vazio ("undefined"), é exibido um erro.
+    // As linha a baixo são para remover esse erro. Básicamente quer dizer: Se o campo "email"
+    // estiver vazio, eu não vou fazer nada "return". 
+    if (!email) {
+      return
+    }
+
+    // Validação para não deixar add um email repetido.
+    if (emailsToInvite.includes(email)) {
+      return console.log('Email já cadastrado.')
+    }
+
+    // Add email no array usando conceito de imutabilidade.
+    setEmailToInvite([...emailsToInvite, email])
+
+    // Limpar o campo/input do formulário.
+    event.currentTarget.reset()
+  }
+
+  function removeEmailFromInvites(emailToRemmove: string) {
+    // criando um novo array de emails
+    // Pega todos os emails do array emailsToInvite que sejam diferentes do "emailToRemomove". 
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemmove)
+    setEmailToInvite(newEmailList)
   }
 
   return (
@@ -83,6 +121,8 @@ export function App() {
           com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
         </p>
       </div>
+
+      {/* ADD EMAIL MODAL */}
       {isGuestModalOpen && (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
           <div className='w-[640px] rounded-lg py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
@@ -96,22 +136,29 @@ export function App() {
               <p className='text-sm text-zinc-400'>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
             </div>
             <div className='flex flex-wrap gap-2'>
-              <div className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
-                <span className='text-zinc-300'>ecthon@gmail.com</span>
-                <button type='button'>
-                  <X className='size-4 text-zinc-400'/>
-                </button>
-              </div>
+              {emailsToInvite.map( email => (
+                <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                  <span className='text-zinc-300'>{email}</span>
+                  <button type='button' onClick={() => removeEmailFromInvites(email)}>
+                    <X className='size-4 text-zinc-400'/>
+                  </button>
+                </div>
+              ))}
             </div>
 
             <div className='w-full h-px bg-zinc-800' />
 
-            <form className='p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+            <form onSubmit={addNewEmailToInvite} className='p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
               <div className='flex items-center px-2 gap-2 flex-1'>
                 <AtSign className='text-zinc-400 size-5' />
-                <input type="text" placeholder='Digite o email do convidado' className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' />
+                <input 
+                  type="email" 
+                  name='email' 
+                  placeholder='Digite o email do convidado' 
+                  className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' 
+                />
               </div>
-              <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+              <button type='submit' className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
                   Convidar
                   <Plus className='size-5'/>
                 </button>
