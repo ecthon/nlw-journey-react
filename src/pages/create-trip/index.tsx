37f3ddc46842f9ccd538eb5f3ae3dbde4ec3f781
ecthon <ecthon@gmail.com>
2024-07-19 22:56:36 -0400

    feature/destination-and-date-header: add get trip details functionality
    - Integrate API call to get trip details from the back-end

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -84,11 +84,6 @@ export function CreateTripPage() {
 
   async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    console.log(destination)
-    console.log(eventStartAndEndDates)
-    console.log(emailsToInvite)
-    console.log(ownerName)
-    console.log(ownerEmail)
 
     if (!destination) {
       return
